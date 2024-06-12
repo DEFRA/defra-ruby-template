aac355871067bb3a5179f31244a9ac775d216d2f
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-06-12 14:49:59 +0100

    Add govuk-frontend.min.js to manifest.js

--- vendor/assets/config/manifest.js
+++ vendor/assets/config/manifest.js
@@ -1,3 +1,4 @@
 //= link defra_ruby_template/defra_ruby_template.css
 //= link defra_ruby_template/defra_ruby_template.js
+//= link defra_ruby_template/govuk-frontend.min.js
 //= link_tree ../images
