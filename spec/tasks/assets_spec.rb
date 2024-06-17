f3d50cc19646d49e237ae3b1b41831b683712373
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-06-17 12:50:16 +0100

    [RUBY-3083] Add RSpec tests for individual asset Rake tasks and reenable tasks before each test

--- spec/tasks/assets_spec.rb
+++ spec/tasks/assets_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# spec/tasks/assets_spec.rb
-
 require "rake"
 require "rspec"
 
@@ -9,10 +7,42 @@ require "rspec"
 load File.expand_path("../../Rakefile", __dir__)
 
 RSpec.describe "Rake tasks" do
-  before(:all) do
-    # Load Rake tasks
-    # Rake.application.rake_require("Rakefile")
-    Rake::Task.define_task(:environment)
+  before(:each) do
+    Rake::Task["minified_css"].reenable
+    Rake::Task["fonts"].reenable
+    Rake::Task["images"].reenable
+    Rake::Task["stylesheets"].reenable
+    Rake::Task["javascripts"].reenable
+  end
+
+  describe "assets:minified_css" do
+    it "runs without error" do
+      expect { Rake::Task["minified_css"].invoke }.not_to raise_error
+    end
+  end
+
+  describe "assets:fonts" do
+    it "runs without error" do
+      expect { Rake::Task["fonts"].invoke }.not_to raise_error
+    end
+  end
+
+  describe "assets:images" do
+    it "runs without error" do
+      expect { Rake::Task["images"].invoke }.not_to raise_error
+    end
+  end
+
+  describe "assets:stylesheets" do
+    it "runs without error" do
+      expect { Rake::Task["stylesheets"].invoke }.not_to raise_error
+    end
+  end
+
+  describe "assets:javascripts" do
+    it "runs without error" do
+      expect { Rake::Task["javascripts"].invoke }.not_to raise_error
+    end
   end
 
   describe "assets" do
