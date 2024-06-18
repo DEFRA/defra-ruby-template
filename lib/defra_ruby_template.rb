d49e574de2f349caacf42fd6f141c409bab7bdbe
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-06-18 13:21:06 +0100

    [RUBY-3223] Update asset precompile list with new favicon and GOV.UK images

--- lib/defra_ruby_template.rb
+++ lib/defra_ruby_template.rb
@@ -9,14 +9,9 @@ module DefraRubyTemplate
         app.config.assets.precompile <<
           [
             "favicon.ico",
-            "govuk-apple-touch-icon-152x152.png",
-            "govuk-apple-touch-icon-167x167.png",
-            "govuk-apple-touch-icon-180x180.png",
-            "govuk-apple-touch-icon.png",
-            "govuk-crest-2x.png",
-            "govuk-crest.png",
-            "govuk-logotype-crown.png",
-            "govuk-mask-icon.svg",
+            "favicon.svg",
+            "govuk-icon-mask.svg",
+            "govuk-icon-180.png",
             "govuk-opengraph-image.png"
           ]
       end
